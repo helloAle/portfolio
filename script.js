79dcda09cacf50e8545c2c7ede62482b557b6a77
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2023-07-13 19:16:03 -0300

    pop up att

---
+++ script.js
@@ -0,0 +1,39 @@
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+            });
+            document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+// espaço
+
+// var prevScrollPos = window.pageYOffset;
+// var header = document.querySelector('.header');
+
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+  
+//   if (prevScrollPos > currentScrollPos) {
+//     header.classList.remove('hide');
+//   } else {
+//     header.classList.add('hide');
+//   }
+  
+//   prevScrollPos = currentScrollPos;
+// }
+ 
