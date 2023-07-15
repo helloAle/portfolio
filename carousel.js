421705fe54d4e7483e910e8d4bae7e592a447a51
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2023-07-14 22:00:02 -0300

    tentativa

---
+++ carousel.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper",{
+    effect: "cards",
+    loop: true,
+    autoplay:{
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    grabCursor: true,
+});
