0e294e8b86e76a22e0344bc25bdc39f0944014b3
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2023-07-15 22:29:53 -0300

    achei o problema

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-// let sections = document.querySelectorAll('section');
-// let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 100;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-//         if (top >= offset && top < offset + height) {
-//             navlinks.forEach(links => {
-//                 links.classList.remove('active');
-//             });
-//             document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
-//         }
-//     });
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+            });
+            document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
+        }
+    });
 
-//     let header = document.querySelector('header');
-//     header.classList.toggle('sticky', window.scrollY > 100);
-// }
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
 
 // espaço
 
